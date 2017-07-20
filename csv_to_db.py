cad503ee90999266a2fae67dab66a33f0a332567
Ryan Cajigal <rcajigal97@gmail.com>
2017-07-19 20:53:35 -0700

    Java/SQL interface started

--- csv_to_db.py
+++ csv_to_db.py
@@ -3,13 +3,16 @@ import sqlite3
 
 conn = sqlite3.connect('exercise.db')
 curs = conn.cursor()
-curs.execute("CREATE TABLE exercises (id, equip_req, ex_name, ex_type, muscle);")
+curs.execute("CREATE TABLE exercises(id, equip_req, ex_name, ex_type, muscle);")
 
 with open('data.csv', 'rb') as open_csv:
 	dict_csv = csv.DictReader(open_csv, delimiter = '\t')
 	to_db = [(i['id'], i['equip_req'], i['ex_name'], i['ex_type'], i['muscle']) for i in dict_csv]
 
 curs.executemany("INSERT INTO exercises (id, equip_req, ex_name, ex_type, muscle) VALUES (?, ?, ?, ?, ?);", to_db)
+
+curs.execute("CREATE TABLE user_database(user_id INTEGER, ex_id INTEGER, set_1 INTEGER, set_1_reps INTEGER, set_2 INTEGER, set_2_reps INTEGER, set_3 INTEGER, set_3_reps INTEGER,set_4 INTEGER, set_4_reps INTEGER, set_5 INTEGER, set_5_reps INTEGER);")
+curs.execute("CREATE TABLE user_list(user_id INTEGER, username TEXT);")
 
 conn.commit()
 conn.close()
---
+++ src/workout_tracker/DBInputter.java
@@ -0,0 +1,57 @@
+package workout_tracker;
+import java.sql.*;
+import java.util.Scanner;
+
+public class DBInputter {
+
+	public static void main(String[] args) {
+		
+		// set up connect
+		Connection conn = null;
+		try {
+			String url = "jdbc:sqlite:exercise.db";
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		// set up scanner and username
+		String name;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("New User? [y/n]");
+		if (input.hasNext() && input.nextLine().equalsIgnoreCase("y")) {
+			System.out.println("Enter your name");
+			name = input.nextLine();
+			int userNumber;
+			try {
+				Statement newUser = conn.createStatement();
+				ResultSet userNum = newUser.executeQuery(
+					"SELECT count(*) AS num FROM user_list;");
+				userNumber = userNum.getInt("num");
+				newUser.executeUpdate("INSERT INTO user_list " + "VALUES (" 
+					+ ++userNumber + ", '" + name + "')");
+			}
+			catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		else {
+			System.out.println("Enter your name");
+			name = input.nextLine();
+		}
+		/*System.out.println("What would you like to do today?");
+		System.out.println("Build a workout [1] or update workout log [2]");
+		int option = input.nextInt();
+		if (option == 1) {
+			System.out.println("Choose muscle(s) to workout");
+			System.out.println("Abs [1]. Back [2], Biceps [3], Chest [4], "
+					+ "Forearm [5], Glutes [6], Shoulders [7], Triceps [8], "
+					+ "Upper Legs [9], Lower Legs [10], or Cardio [11]");
+		} else {
+			System.out.println("Please input excercise name");
+		}
+		*/
+		input.close();
+	}
+}
