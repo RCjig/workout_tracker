bacdce43ac76e4ca30f6ef61bf64b3a36d017fe8
Ryan Cajigal <rcajigal97@gmail.com>
2017-07-19 21:29:24 -0700

    TODO figure out cleaner option select

--- src/workout_tracker/DBInputter.java
+++ src/workout_tracker/DBInputter.java
@@ -20,10 +20,15 @@ public class DBInputter {
 		int userNumber;
 		Scanner input = new Scanner(System.in);
 		
+		// new user
 		System.out.println("New User? [y/n]");
 		if (input.hasNext() && input.nextLine().equalsIgnoreCase("y")) {
+			
+			// get name and insert into table in database
 			System.out.println("Enter your name");
 			name = input.nextLine();
+			
+			// TODO add duplicate user prevention
 			try {
 				Statement newUser = conn.createStatement();
 				ResultSet userNum = newUser.executeQuery(
@@ -31,36 +36,47 @@ public class DBInputter {
 				userNumber = userNum.getInt("num");
 				newUser.executeUpdate("INSERT INTO user_list " + "VALUES (" 
 					+ ++userNumber + ", '" + name + "')");
-			}
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				System.out.println(e.getMessage());
 			}
 		}
 		else {
+			// returning user
 			System.out.println("Enter your name");
 			name = input.nextLine();
+			
+			// obtain user id number
 			try {
 				Statement retUser = conn.createStatement();
 				ResultSet userNum = retUser.executeQuery(
 					"SELECT user_id AS num FROM user_list WHERE username =='"
 					+ name + "';");
-				System.out.print(userNum.getInt("num"));
+				userNumber = userNum.getInt("num");
 			} catch (SQLException e) {
 				System.out.println(e.getMessage());
 			}
 		}
-		/*System.out.println("What would you like to do today?");
+		
+		// program options
+		System.out.println("What would you like to do today?");
 		System.out.println("Build a workout [1] or update workout log [2]");
 		int option = input.nextInt();
+		
+		// build a workout / recommender
 		if (option == 1) {
-			System.out.println("Choose muscle(s) to workout");
-			System.out.println("Abs [1]. Back [2], Biceps [3], Chest [4], "
-					+ "Forearm [5], Glutes [6], Shoulders [7], Triceps [8], "
-					+ "Upper Legs [9], Lower Legs [10], or Cardio [11]");
+			System.out.println("Choose muscle(s) to workout:");
+			System.out.println("Abs [a]. Back [b], Biceps [c], Chest [d], "
+					+ "Forearm [e], Glutes [f], Shoulders [g], Triceps [h], "
+					+ "Upper Legs [i], Lower Legs [j], or Cardio [k]");
+			
+			//String options = input.nextLine();
+			//boolean a, b, c, d, e, f, g, h, i, j, k;
+			
 		} else {
+			// update personal log in database
 			System.out.println("Please input excercise name");
 		}
-		*/
+		
 		input.close();
 	}
 }
